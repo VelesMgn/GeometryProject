d6b770fe72d380a0b85d0f606db53c38d370ce8b
Vladimir Stolyarov <laserjet4l.mgn@gmail.com>
2025-05-25 13:10:47 +0500

    add ThreeDimensionalShapes, dependencyManagement

--- GeometryApp/src/main/java/com/geometry/Main.java
+++ GeometryApp/src/main/java/com/geometry/Main.java
@@ -6,6 +6,7 @@ import org.example.Triangle;
 
 public class Main {
     public static void main(String[] args) {
+        // 2D фигуры
         Circle circle = new Circle(5);
         Rectangle rectangle = new Rectangle(4, 6);
         Triangle triangle = new Triangle(3, 4, 5);
@@ -27,5 +28,16 @@ public class Main {
 
         boolean areasEqual = ShapeComparator.compareAreas(circle, rectangle);
         System.out.println("Are areas equal? " + areasEqual);
+
+        System.out.println();
+
+        // 3D фигуры
+        Cube cube = new Cube(3);
+        Sphere sphere = new Sphere(4);
+
+        System.out.println("Cube volume: " + cube.getVolume());
+        System.out.println("Cube area: " + cube.getSurfaceArea());
+        System.out.println("Sphere surface area: " + sphere.getSurfaceArea());
+        System.out.println("Sphere volume: " + sphere.getVolume());
     }
 }
---
+++ ThreeDimensionalShapes/src/main/java/com/geometry/Cube.java
@@ -0,0 +1,17 @@
+package com.geometry;
+
+public class Cube {
+    private final double side;
+
+    public Cube(double side) {
+        this.side = side;
+    }
+
+    public double getVolume() {
+        return side * side * side;
+    }
+
+    public double getSurfaceArea() {
+        return 6 * side * side;
+    }
+}
