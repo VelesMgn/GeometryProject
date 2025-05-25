b315b25364a2a836758ae4bec5bbbedf7cce2974
Vladimir Stolyarov <laserjet4l.mgn@gmail.com>
2025-05-25 12:31:16 +0500

    add GeometryLibrary, GeometryApp

---
+++ GeometryApp/src/main/java/com/geometry/Main.java
@@ -0,0 +1,23 @@
+package com.geometry;
+
+import org.example.Circle;
+import org.example.Rectangle;
+import org.example.Triangle;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        Rectangle rectangle = new Rectangle(4, 6);
+        Triangle triangle = new Triangle(3, 4, 5);
+
+        System.out.println("Circle area: " + circle.getArea());
+        System.out.println("Rectangle area: " + rectangle.getArea());
+        System.out.println("Triangle area: " + triangle.getArea());
+
+        System.out.println();
+
+        System.out.println("Circle perimeter: " + circle.getPerimeter());
+        System.out.println("Rectangle perimeter: " + rectangle.getPerimeter());
+        System.out.println("Triangle perimeter: " + triangle.getPerimeter());
+    }
+}
